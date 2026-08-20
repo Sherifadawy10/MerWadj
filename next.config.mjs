2b74151ad2857f801506dad1bbce88afb01e0bde
Shigalevsk <shigalevsk@gmail.com>
2026-08-20 23:36:24 +0300

    add files

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "medwadj.dialen.com.ua",
       },
+      {
+        protocol: "https",
+        hostname: "1196411.us17.myftpupload.com",
+      },
+      {
+        protocol: "https",
+        hostname: "merwadj.com",
+      },
     ],
   },
 };
